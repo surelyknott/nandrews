24f85bb1e8629548f61caf9cad37367fda9f6f4b
Jack Knott <jackhknott@hotmail.co.uk>
2026-04-17 18:05:16 +0100

    custom google reviews section

---
+++ src/_data/reviews.js
@@ -0,0 +1,32 @@
+module.exports = {
+  rating: '4.9',
+  count: 23,
+  googleUrl: 'https://www.google.com/search?sca_esv=e8fdf001dc504c87&sxsrf=ANbL-n4L648tXT8FI3POA504fhlwZSnZAg:1776442810659&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOQ2CxwjFJkKUR5RHkHstikauhP0pjkp8P7I5ePvHTkCNqowQlojQvwmJWSLYLi3MUZ4LPhAzuVY5Sjuj-OHZqD9iLOBX-okaAiJoYDd6BfmV3Pfr7w%3D%3D&q=Nandrews+Garage+Ltd+Reviews&sa=X&ved=2ahUKEwitwMWVpfWTAxVpUUEAHYtYMgUQ0bkNegQIIxAH&biw=1512&bih=762&dpr=2',
+  writeReviewUrl: 'https://www.google.com/search?sca_esv=e8fdf001dc504c87&sxsrf=ANbL-n4Jybj04gHl_VYsX6uWXycwL6CQfg:1776444737907&si=AL3DRZEsmMGCryMMFSHJ3StBhOdZ2-6yYkXd_doETEE1OR-qOQ2CxwjFJkKUR5RHkHstikauhP0pjkp8P7I5ePvHTkCNqowQlojQvwmJWSLYLi3MUZ4LPhAzuVY5Sjuj-OHZqD9iLOBX-okaAiJoYDd6BfmV3Pfr7w%3D%3D&q=Nandrews+Garage+Ltd+Reviews&sa=X&ved=2ahUKEwi7rsOsrPWTAxUiSEEAHTEUDyQQ0bkNegQIIxAH&cshid=1776444782421599&biw=1512&bih=762&dpr=2#lrd=0x4874769ee8ff20d1:0xfa8c61ed7746e238,3,,,,',
+  items: [
+    {
+      name: 'LanaS SoCoDigi',
+      dateLabel: '1 month ago',
+      initials: 'L',
+      excerpt: 'So, I brought my car in and they found the issue immediately. They are very fa...'
+    },
+    {
+      name: 'Shane Doyle',
+      dateLabel: '5 months ago',
+      initials: 'S',
+      excerpt: 'Terrific garage. Left my Audi estate in, to have the rear wiper motor replaced. The...'
+    },
+    {
+      name: 'Carla Hall',
+      dateLabel: '5 months ago',
+      initials: 'C',
+      excerpt: 'Phoned about my headlights and spoke to Mitchell who said I could bring my car dow...'
+    },
+    {
+      name: 'Chris Heath',
+      dateLabel: '1 year ago',
+      initials: 'C',
+      excerpt: 'Brilliant place and they always ave been. Just sorted my exhaust problems caused by...'
+    }
+  ]
+};
